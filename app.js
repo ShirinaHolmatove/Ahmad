e9cd4e66ea0c512f7af0a8225d7d029b155622ec
ShirinaHolmatove <ahmadbaba_@outlook.com>
2023-06-13 04:19:08 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+
+
+const express = require('express');
+const path = require('path');
+
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+
+
+const methodOverride = require('method-override');
+
+
+const app = express();
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+app.get('/resume', (req, res) => {
+    res.render('resume')
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+});
+
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening to npm port ${port}`));
+
---
+++ public/js/index.js
@@ -0,0 +1,49 @@
+const fname = document.getElementById('fname');
+const femail = document.getElementById('femail');
+const fsubject = document.getElementById('fsubject');
+const fmessage = document.getElementById('fmessage');
+const submit = document.getElementById('submit');
+
+submit.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let ebody = `
+    <h1>First Name: </h1>${fname.value}
+    <br>
+    <h1>message: </h1>${fmessage.value}
+    <br>
+    <h1>email: </h1>${femail.value}
+    <br>
+    <h1>subject: </h1>${fsubject.value}
+    `;
+
+    Email.send({
+        SecureToken: "73ea80c4-d38c-49f3-9909-d901abae4b86", //add your token here
+        To: 'ahmadbaba_@outlook.com',
+        From: "ahmadadnanbaba@gmail.com",
+        Subject: "This is the subject",
+        Body: ebody
+    }).then(
+        message => alert(message)
+    );
+
+
+    /*function sendEmail() {
+        Email.send({
+            Host: "smtp.elasticemail.com",
+            Username: "ahmadadnanbaba@gmail.com",
+            Password: "B23146A35F453407327D008D4E04DEEE81DB",
+            To: 'ahmadbaba_@outlook.com',
+            From: 'ahmadadnanbaba@gmail.com', // put a real email address that is verified
+            Port: '2525',
+            Subject: "This is the subject",
+            Body: "And this is the body"
+
+        }).then(
+            message => {
+                alert("mail sent successfully");
+                console.log(message);
+            }
+        );
+
+    }*/
+});
